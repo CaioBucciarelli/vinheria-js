f0b071e89428fc1d62310ae19aba34741b1bbb07
CaioBucciarelli <caio.bucciarelli@gmail.com>
2024-04-29 19:02:35 -0300

    Todos os arquivos

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/************************************************************ VERIFICAÇÃO DE IDADE ************************************************************/
+/*
+function verificarIdade() {
+  while (true) {
+    idade = prompt("Por favor, informe sua idade:");
+    if (isNaN(idade) || idade.trim() === '') {
+        alert("Por favor, informe uma idade válida.");
+    } else {
+      idade = parseInt(idade);
+      if (idade >= 18) {
+        window.location.href;
+      } else {
+        window.location.href = "https://www.ssp.sc.gov.br/index.php/programas/proerd";
+      }
+      break;
+    }
+  }
+} 
+
+window.onload = function() {
+  verificarIdade();
+};
+*/
